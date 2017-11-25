a26f15fee38117e03821d1c556fafee5633c2242
Diogo Dores <diogo.dores@gmail.com>
2017-11-25 12:46:38 +0000

    src file added

---
+++ scr.lua
@@ -0,0 +1,16 @@
+function _init()
+cls()
+--apenas corre uma vez.
+--primeira funcao a ser corrida
+end
+
+function move()
+end
+
+function _update()
+move()
+end
+
+function _draw()
+--sprites e whatever
+end
